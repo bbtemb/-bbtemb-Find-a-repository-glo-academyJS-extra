b474a86560bb7ac43d8a5edb18d90c4ba1228172
bbtemb <bbtemb@gmail.com>
2024-02-08 15:29:21 +0500

    поправил урок 7

--- script.js
+++ script.js
@@ -14,31 +14,19 @@ const week = [
 ];
 
 const now = function () {
-  switch (new Date().getDay()) {
-    case 1:
-      return 'Понедельник';
-    case 2:
-      return 'Вторник';
-    case 3:
-      return 'Среда';
-    case 4:
-      return 'Четверг';
-    case 5:
-      return 'Пятница';
-    case 6:
-      return 'Суббота';
-    case 0:
-      return 'Воскресенье';
-  }
+  let dayId = new Date().getDay();
+  return dayId === 0 ? week[6] : week[dayId - 1];
 };
 
+const currentDay = now();
+
 const prettyWeek = week.map(function (item) {
-  if (item === now()) {
-    return (item = '<p><b>' + item + '</b></p>');
+  if (item === currentDay) {
+    return '<p><b>' + item + '</b></p>';
   } else if (item === 'Суббота' || item === 'Воскресенье') {
-    return (item = '<p><i>' + item + '</i></p>');
+    return '<p><i>' + item + '</i></p>';
   } else {
-    return (item = '<p>' + item + '</p>');
+    return '<p>' + item + '</p>';
   }
 });
 
