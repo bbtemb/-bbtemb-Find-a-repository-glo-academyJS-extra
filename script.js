aaa81ea7e88a49a0260ba1067c07ea8de7cd5a49
bbtemb <bbtemb@gmail.com>
2024-02-16 03:35:16 +0500

    добавил все пункты

--- script.js
+++ script.js
@@ -3,44 +3,116 @@
 let outputString = '';
 let arrayOutput = document.querySelector('#array__output');
 
-function getCurrentDateTime() {
-  const daysOfWeek = [
-    'Воскресенье',
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота',
-  ];
-  const monthsOfYear = [
-    'января',
-    'февраля',
-    'марта',
-    'апреля',
-    'мая',
-    'июня',
-    'июля',
-    'августа',
-    'сентября',
-    'октября',
-    'ноября',
-    'декабря',
-  ];
+let formattedDateA = '';
+let formattedDateB = '';
 
-  const currentDate = new Date();
-  const weekday = daysOfWeek[currentDate.getDay()];
-  const day = currentDate.getDate();
-  const month = monthsOfYear[currentDate.getMonth()];
-  const year = currentDate.getFullYear();
-  const hours = currentDate.getHours();
-  const minutes = currentDate.getMinutes();
-  const seconds = currentDate.getSeconds();
+let currentDate = '';
+let weekday = 0;
+let day = 0;
+let month = '';
+let monthNumber = 0;
+let year = 0;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
 
-  const formattedDate = `Сегодня ${weekday}, ${day} ${month} ${year} года, ${hours} час ${minutes} минут ${seconds} секунды`;
+const daysOfWeek = [
+  'Воскресенье',
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+];
+const monthsOfYear = [
+  'января',
+  'февраля',
+  'марта',
+  'апреля',
+  'мая',
+  'июня',
+  'июля',
+  'августа',
+  'сентября',
+  'октября',
+  'ноября',
+  'декабря',
+];
 
-  return formattedDate;
+const hourDeclensionArr = ['час', 'часа', 'часов'];
+const minuteDeclensionArr = ['минута', 'минуты', 'минут'];
+const secondDeclensionArr = ['секунда', 'секунды', 'секунд'];
+
+let hourDeclension = '';
+let minuteDeclension = '';
+let secondDeclension = '';
+
+function getValues() {
+  currentDate = new Date();
+  weekday = daysOfWeek[currentDate.getDay()];
+  day = currentDate.getDate();
+  month = monthsOfYear[currentDate.getMonth()];
+  monthNumber = currentDate.getMonth() + 1;
+  year = currentDate.getFullYear();
+  hours = currentDate.getHours();
+  minutes = currentDate.getMinutes().toString();
+  seconds = currentDate.getSeconds().toString();
 }
 
-outputString = getCurrentDateTime();
-arrayOutput.innerHTML = outputString;
+function getHourDeclension() {
+  if (hours === 1 || hours === 21) {
+    hourDeclension = hourDeclensionArr[0];
+  } else if (
+    hours === 2 ||
+    hours === 3 ||
+    hours === 4 ||
+    hours === 22 ||
+    hours === 23
+  ) {
+    hourDeclension = hourDeclensionArr[1];
+  } else {
+    hourDeclension = hourDeclensionArr[2];
+  }
+  return hourDeclension;
+}
+function getDeclension(elem, arr) {
+  let declension = '';
+  if (elem > 4 && elem < 21) {
+    declension = arr[2];
+  } else if (elem.charAt(elem.length - 1) == 1) {
+    declension = arr[0];
+  } else if (
+    elem.charAt(elem.length - 1) == 2 ||
+    elem.charAt(elem.length - 1) == 3 ||
+    elem.charAt(elem.length - 1) == 4
+  ) {
+    declension = arr[1];
+  } else declension = arr[2];
+
+  return declension;
+}
+
+function twoDigits(num) {
+  return ('0' + num).slice(-2);
+}
+
+function getformattedDateA() {
+  formattedDateA = `Сегодня ${weekday}, ${day} ${month} ${year} года, ${hours} ${getHourDeclension()} ${minutes} ${getDeclension(
+    minutes,
+    minuteDeclensionArr
+  )} ${seconds} ${getDeclension(seconds, secondDeclensionArr)}`;
+  return formattedDateA;
+}
+function getformattedDateB() {
+  formattedDateB = `${twoDigits(day)}.${twoDigits(
+    monthNumber
+  )}.${year} - ${twoDigits(hours)}:${twoDigits(minutes)}:${twoDigits(seconds)}`;
+  return formattedDateB;
+}
+
+setInterval(() => {
+  getValues();
+  outputString = getformattedDateA() + '<br>' + getformattedDateB();
+  arrayOutput.innerHTML = outputString;
+}, 1000);
